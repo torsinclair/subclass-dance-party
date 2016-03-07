07713fc5fa91d18fd29db9ac268146355e7a67d7
torsinclair <tor@romanesco.se>
2016-03-07 15:28:48 -0800

    Complete first attempt of pseudoclassical refactor

--- src/dancer.js
+++ src/dancer.js
@@ -1,3 +1,4 @@
+/*
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
 
@@ -30,3 +31,35 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+*/
+
+var makeDancer = function(top, left, timeBetweenSteps){
+
+  // use jQuery to create an HTML <span> tag
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(top, left);
+
+};
+
+makeDancer.prototype.step = function(){
+    // the basic dancer doesn't do anything interesting at all on each step,
+    // it just schedules the next step
+    setTimeout(function(){
+      this.step.call(this)}, timeBetweenSteps);
+
+}
+
+makeDancer.prototype.setPosition = function(top, left) {
+    // Use css top and left properties to position our <span> tag
+    // where it belongs on the page. See http://api.jquery.com/css/
+    //
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    this.$node.css(styleSettings);
+  };
+
+
+
