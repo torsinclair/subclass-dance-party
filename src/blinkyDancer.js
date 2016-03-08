4b4ac68be20464b9eed86747d777acc97da21582
Tor Sinclair <tor@romanesco.se>
2016-03-07 21:46:42 -0800

    Refactor blinkyDancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -26,12 +26,15 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps){
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
+  
   makeDancer.prototype.step.call(this);
-  //this.step();
+
+  //this.step(); jquery add class remove class css transitions
     
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
