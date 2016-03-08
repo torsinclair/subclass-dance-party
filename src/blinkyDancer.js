70d8252b81f3d9df5bed11271a5deebcdfaef2fc
torsinclair <tor@romanesco.se>
2016-03-07 17:34:54 -0800

    Be refactored using the pseudoclassical approach

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -21,18 +21,22 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 */
 
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  makeDancer.call(this);
-  this.oldStep = blinkyDancer.step;
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  //this.step();
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    this.oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    this.blinkyDancer.$node.toggle();  
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  //this.step();
+    
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+    
+  //this.blinkyDancer.$node.toggle();
+  this.$node.toggle();
 };
